081586387f7241a25ce3b8ac0efc0b4f4380b737
Victor <vperezco@uc.cl>
2019-06-03 20:58:43 -0400

    background

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,10 +18,12 @@ Rails.application.config.assets.paths << Rails.root.join('vendor','js')
 Rails.application.config.assets.paths << Rails.root.join('vendor','bootstrap','js')
 
 
-Rails.application.config.assets.paths << Rails.root.join('vendor','img','backgrounds')
+Rails.application.config.assets.paths << Rails.root.join('vendor','img')
 
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( *.jpg, *.png )
